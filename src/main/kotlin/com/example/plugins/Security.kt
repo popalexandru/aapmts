bdc6afe0c1efac0ccae5f93117984aa86b52b471
popalexandru <popalexandru1197@gmail.com>
2021-11-15 18:02:28 +0200

    Prepare project for deployment

---
+++ src/main/kotlin/com/example/plugins/Security.kt
@@ -0,0 +1,67 @@
+package com.example.plugins
+
+import io.ktor.auth.*
+import io.ktor.util.*
+import io.ktor.auth.jwt.*
+import com.auth0.jwt.JWT
+import com.auth0.jwt.JWTVerifier
+import com.auth0.jwt.algorithms.Algorithm
+import io.ktor.application.*
+import io.ktor.response.*
+import io.ktor.request.*
+import io.ktor.routing.*
+
+fun Application.configureSecurity() {
+    
+    authentication {
+            jwt {
+                val jwtAudience = environment.config.property("jwt.audience").getString()
+                realm = environment.config.property("jwt.realm").getString()
+                verifier(
+                    JWT
+                        .require(Algorithm.HMAC256("secret"))
+                        .withAudience(jwtAudience)
+                        .withIssuer(environment.config.property("jwt.domain").getString())
+                        .build()
+                )
+                validate { credential ->
+                    if (credential.payload.audience.contains(jwtAudience)) JWTPrincipal(credential.payload) else null
+                }
+            }
+        }
+    authentication {
+    		basic(name = "myauth1") {
+    			realm = "Ktor Server"
+    			validate { credentials ->
+    				if (credentials.name == credentials.password) {
+    					UserIdPrincipal(credentials.name)
+    				} else {
+    					null
+    				}
+    			}
+    		}
+    
+    	    form(name = "myauth2") {
+    	        userParamName = "user"
+    	        passwordParamName = "password"
+    	        challenge {
+    	        	/**/
+    			}
+    	    }
+    	}
+
+    routing {
+        authenticate("myauth1") {
+            get("/protected/route/basic") {
+                val principal = call.principal<UserIdPrincipal>()!!
+                call.respondText("Hello ${principal.name}")
+            }
+        }
+        authenticate("myauth1") {
+            get("/protected/route/form") {
+                val principal = call.principal<UserIdPrincipal>()!!
+                call.respondText("Hello ${principal.name}")
+            }
+        }
+    }
+}
